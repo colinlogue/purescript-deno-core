e0276f44dc015a39fa68fb7458c174ad116c63d0
Colin Logue <colin.git@fastmail.com>
2025-05-23 20:00:48 -0700

    fix: Add missing FFI file for Deno module

---
+++ src/Deno.js
@@ -0,0 +1,23 @@
+export const _mkdir = (opts, path, onSuccess, onError) => {
+    Deno.mkdir(path, opts)
+        .then(onSuccess)
+        .catch(onError);
+};
+export const _readTextFile = (path, onSuccess, onError) => {
+    Deno.readTextFile(path)
+        .then(onSuccess)
+        .catch(onError);
+};
+export const _writeTextFile = (opts, path, data, onSuccess, onError) => {
+    Deno.writeTextFile(path, data, opts)
+        .then(onSuccess)
+        .catch(onError);
+};
+export const _open = (opts, path, onSuccess, onError) => {
+    Deno.open(path, opts)
+        .then(onSuccess)
+        .catch(onError);
+};
+export const _chdir = (path) => {
+    Deno.chdir(path);
+};
