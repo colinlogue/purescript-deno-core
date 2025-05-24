a7037096c9b3f4e2f7841d3f0f854460e7387bc3
Colin Logue <colin.git@fastmail.com>
2025-05-23 20:53:58 -0700

    test: Add testing framework
    Note that since spec-deno depends on this library, we have to put deno-core in extraPackages and point it at the project directory. Which feels weird, but it seems to work.

--- test/Test/Main.purs
+++ test/Test/Main.purs
@@ -4,9 +4,14 @@ import Prelude
 
 import Effect (Effect)
 import Effect.Class.Console (log)
+import Test.Spec (describe, it)
+import Test.Spec.Reporter (consoleReporter)
+import Test.Spec.Runner.Deno (runSpecAndExitProcess)
 
 main :: Effect Unit
-main = do
-  log "🍕"
-  log "You should add some tests."
-
+main = runSpecAndExitProcess [ consoleReporter ] spec
+  where
+    spec = do
+      describe "Test.Main" do
+        it "should run the test suite" do
+          log "Hello, world!"
