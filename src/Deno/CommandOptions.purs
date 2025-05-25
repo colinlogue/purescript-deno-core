1d27a4cb88827b464c2f919d85999aab8a8353cc
Colin Logue <colin.git@fastmail.com>
2025-05-25 07:48:07 -0700

    feat: Add bindings for remaining Deno.CommandOptions interface

--- src/Deno/CommandOptions.purs
+++ src/Deno/CommandOptions.purs
@@ -1,16 +1,76 @@
-module Deno.CommandOptions where
+module Deno.CommandOptions
+  ( CommandOptions
+  , StdioOption(..)
+  , class IsCommandEnv
+  , empty
+  , args
+  , cwd
+  , clearEnv
+  , env
+  , uid
+  , gid
+  , stdin
+  , stdout
+  , stderr
+  , windowsRawArguments
+  ) where
 
 import Prelude
 
 import Data.Function.Uncurried (Fn2, runFn2)
+import Data.IsStringOrUrl (class IsStringOrUrl, StringOrUrl, toStringOrUrl)
+import JS.Fetch.AbortController (AbortSignal)
+import Prim.RowList (class RowToList, RowList)
+import Prim.RowList as RowList
+import Unsafe.Coerce (unsafeCoerce)
+
 
 foreign import data CommandOptions :: Type
+
+data StdioOption = Piped | Inherit | Null
+
+stdioOptionToString :: StdioOption -> String
+stdioOptionToString = case _ of
+  Piped -> "piped"
+  Inherit -> "inherit"
+  Null -> "null"
 
 foreign import empty :: CommandOptions
 
 foreign import args :: Array String -> CommandOptions
 
-foreign import env :: forall r. Record r -> CommandOptions
+foreign import _cwd :: StringOrUrl -> CommandOptions
+
+cwd :: forall a. IsStringOrUrl a => a -> CommandOptions
+cwd path = _cwd (toStringOrUrl path)
+
+foreign import clearEnv :: Boolean -> CommandOptions
+
+foreign import _env :: CommandEnv -> CommandOptions
+
+env :: forall r. IsCommandEnv r => Record r -> CommandOptions
+env = unsafeCoerce >>> _env
+
+foreign import uid :: Int -> CommandOptions
+
+foreign import gid :: Int -> CommandOptions
+
+foreign import signal :: AbortSignal -> CommandOptions
+
+foreign import _stdin :: String -> CommandOptions
+foreign import _stdout :: String -> CommandOptions
+foreign import _stderr :: String -> CommandOptions
+
+stdin :: StdioOption -> CommandOptions
+stdin opt = _stdin (stdioOptionToString opt)
+
+stdout :: StdioOption -> CommandOptions
+stdout opt = _stdout (stdioOptionToString opt)
+
+stderr :: StdioOption -> CommandOptions
+stderr opt = _stderr (stdioOptionToString opt)
+
+foreign import windowsRawArguments :: Boolean -> CommandOptions
 
 foreign import combine :: Fn2 CommandOptions CommandOptions CommandOptions
 
@@ -19,3 +79,11 @@ instance Semigroup CommandOptions where
 
 instance Monoid CommandOptions where
   mempty = empty
+
+foreign import data CommandEnv :: Type
+
+class IsCommandEnv (r :: Row Type)
+class IsCommandEnvRL (rl :: RowList Type)
+instance IsCommandEnvRL RowList.Nil
+instance (IsCommandEnvRL tail) => IsCommandEnvRL (RowList.Cons label String tail)
+instance (RowToList r rl, IsCommandEnvRL rl) => IsCommandEnv r
--- src/Deno/CommandOptions.ts
+++ src/Deno/CommandOptions.ts
@@ -6,8 +6,40 @@ export const args: Fn1<string[], Deno.CommandOptions> = (value) => ({
   args: value,
 });
 
+export const _cwd: Fn1<string | URL, Deno.CommandOptions> = (value) => ({
+  cwd: value,
+});
+
+export const clearEnv: Fn1<boolean, Deno.CommandOptions> = (value) => ({
+  clearEnv: value,
+});
+
 export const env: Fn1<Record<string, string>, Deno.CommandOptions> = (value) => ({
   env: value,
+});
+
+export const uid: Fn1<number, Deno.CommandOptions> = (value) => ({
+  uid: value,
+});
+
+export const gid: Fn1<number, Deno.CommandOptions> = (value) => ({
+  gid: value,
+});
+
+export const _stdin: Fn1<string, Deno.CommandOptions> = (value) => ({
+  stdin: value as "piped" | "inherit" | "null",
+});
+
+export const _stdout: Fn1<string, Deno.CommandOptions> = (value) => ({
+  stdout: value as "piped" | "inherit" | "null",
+});
+
+export const _stderr: Fn1<string, Deno.CommandOptions> = (value) => ({
+  stderr: value as "piped" | "inherit" | "null",
+});
+
+export const windowsRawArguments: Fn1<boolean, Deno.CommandOptions> = (value) => ({
+  windowsRawArguments: value,
 });
 
 export const combine: Fn2<
